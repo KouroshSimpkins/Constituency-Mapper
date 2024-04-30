22902b2570d68f248d3360f83136e206240e6ff6
Kourosh <kouroshsimpkins@gmail.com>
2024-04-30 17:41:28 +0100

    Functional System for dbInteraction

--- dbInteraction.py
+++ dbInteraction.py
@@ -1,13 +1,9 @@
 # Acts as a go between for the database and the OpenStreetMap street names.
 
 import mysql.connector
-import geojson
+from mysql.connector import errorcode
 from GEOJSONtoPOLYGON import generate_overpass_queries
 import requests
-
-
-all_queries = generate_overpass_queries('Geojson_data/2010_constituencies___england__south_.geojson')
-london_westminster_query = list(filter(lambda q: q['Name'] == 'Cities of London & Westminster', all_queries))
 
 
 def query_overpass_api(overpass_query):
@@ -51,23 +47,69 @@ def extract_street_names(overpass_response):
     return list(street_names_set)
 
 
-try:
-    conn = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='change-me',
-        database='mysql'
-    )
-    print("Connected to MySQL server")
-except mysql.connector.Error as e:
-    print(f"Error connecting to MySQL database: {e}")
+def generate_constituency_database(constituency_name):
+    """
+    Generates a database with the name of the constituency,
+    to help isolate the data for each parliamentary constituency.
+
+    :param constituency_name:
+    :return:
+    """
+
+    try:
+        conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='change-me',
+        )
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Invalid username or password")
+        else:
+            print(f"Error connecting to the MySQL server: {err}")
+        exit(1)
+
+    cursor = conn.cursor()
+
+    db_name = str(constituency_name)
+    try:
+        cursor.execute(f"CREATE DATABASE {db_name}")
+        print(f"Database '{db_name}' created successfully.")
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_DB_CREATE_EXISTS:
+            print(f"Database '{db_name}' already exists.")
+        else:
+            print(f"Error creating database: {err}")
+
+    cursor.execute(f"USE {db_name}")
+
+    table_queries = [
+        "CREATE TABLE Leafletters (user_id CHAR(36) PRIMARY KEY DEFAULT (UUID()), username VARCHAR(40), email_address VARCHAR(100), phone_number VARCHAR(20));" # noqa
+        "CREATE TABLE Roads (road_id CHAR(36) PRIMARY KEY DEFAULT (UUID()), road_name VARCHAR(255), last_visited DATETIME, visited BOOLEAN, visited_by CHAR(36), FOREIGN KEY (visited_by) REFERENCES Leafletters(user_id));" # noqa
+    ]
+
+    for table_query in table_queries:
+        try:
+            cursor.execute(table_query)
+            print(f"Table created successfully.")
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print(f"Table already exists.")
+            else:
+                print(f"Error creating table: {err}")
+
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM Roads")
-results = cursor.fetchall()
-for row in results:
-    print(row)
+if __name__ == '__main__':
+    all_queries = generate_overpass_queries('Geojson_data/2010_constituencies___england__south_.geojson')
+    london_westminster_query = list(filter(lambda q: q['Name'] == 'Cities of London & Westminster', all_queries))
 
-cursor.close()
-conn.close()
+    overpass_response = query_overpass_api(london_westminster_query[0]['object'])
+    street_names_list = extract_street_names(overpass_response)
+
+    generate_constituency_database(london_westminster_query[0]['Name'])
+
+
